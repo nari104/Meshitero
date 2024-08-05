af32817ed9799291f02cc8eeef20a7f76e5a704a
nari104 <172471497+nari104@users.noreply.github.com>
2024-08-05 20:19:15 +0000

    [update] 2-1-11

---
+++ db/migrate/20240803165808_create_post_images.rb
@@ -0,0 +1,13 @@
+class CreatePostImages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :post_images, id:false do |t|
+
+      t.integer :id, null: false, primary_key: true
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
